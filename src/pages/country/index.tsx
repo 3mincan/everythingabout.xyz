7003e150fdf826b6f805fe81eb60df65eec537cd
Emincan Demirkaya <emincan@fireflyon.com>
2020-08-05 10:42:07 +0300

    fix visual bugs

--- src/pages/country/index.tsx
+++ src/pages/country/index.tsx
@@ -4,187 +4,227 @@ import baseURL from "../../api";
 import { Loader } from "../loader/Loader";
 import { useParams } from "react-router-dom";
 import { Nav } from "../nav";
-import { Footer } from "../footer"
+import { Footer } from "../footer";
 import { Container, Col, Row, Card } from "react-bootstrap";
-import './style.scss';
+import "./style.scss";
 
 type State = {
-    name?: string,
-    isLoading?: boolean,
-    error?: boolean,
-    data?: Data[],
+  name?: string;
+  isLoading?: boolean;
+  error?: boolean;
+  data?: Data[];
 };
 
 type UrlParamsType = {
-    name: string;
+  name: string;
 };
 
 type Data = {
-    name: string,
-    topLevelDomain: Array<string>,
-    alpha2Code: string,
-    alpha3Code: string,
-    callingCodes: Array<string>,
-    capital: string,
-    altSpellings: Array<string>,
-    region: string,
-    subregion: string,
-    population: number,
-    latlng: Array<number>,
-    demonym: string,
-    area: number,
-    gini: number,
-    timezones: Array<string>,
-    borders: BordersType,
-    nativeName: string,
-    numericCode: string,
-    currencies: CurrencyType,
-    languages: Array<object>,
-    translations: Translations,
-    flag: string,
-    regionalBlocs: Array<object>,
-    cioc: string,
-    currentCurreny: string,
+  name: string;
+  topLevelDomain: Array<string>;
+  alpha2Code: string;
+  alpha3Code: string;
+  callingCodes: Array<string>;
+  capital: string;
+  altSpellings: Array<string>;
+  region: string;
+  subregion: string;
+  population: number;
+  latlng: Array<number>;
+  demonym: string;
+  area: number;
+  gini: number;
+  timezones: Array<string>;
+  borders: BordersType;
+  nativeName: string;
+  numericCode: string;
+  currencies: CurrencyType;
+  languages: Array<object>;
+  translations: Translations;
+  flag: string;
+  regionalBlocs: Array<object>;
+  cioc: string;
+  currentCurreny: string;
 };
 
 type Currency = {
-    code: string,
-    name: string,
-    symbol: string,
-}
+  code: string;
+  name: string;
+  symbol: string;
+};
 
 type CurrencyType = {
-    [key: string]: Currency;
-}
+  [key: string]: Currency;
+};
 
 type BordersType = {
-    [key: string]: string;
-}
+  [key: string]: string;
+};
 
 type Translations = {
-    br: string,
-    de: string,
-    es: string,
-    fr: string,
-    ja: string,
-    it: string,
-    fa: string,
-    pt: string,
-    nl: string,
-}
+  br: string;
+  de: string;
+  es: string;
+  fr: string;
+  ja: string;
+  it: string;
+  fa: string;
+  pt: string;
+  nl: string;
+};
 
 export const Country: FC<State> = () => {
+  const [{ data, isLoading }, setState] = useState<State>({
+    data: [],
+    isLoading: false,
+  });
 
-    const [{ data, isLoading }, setState] = useState<State>({
-        data: [],
+  const urlParams = useParams<UrlParamsType>();
+
+  const fetchCountry = useCallback(async () => {
+    setState((state) => ({ ...state, isLoading: true }));
+    try {
+      const { data } = await Axios.get(
+        baseURL + "alpha/" + `${urlParams.name}`
+      );
+      setState((state) => ({
+        ...state,
+        data: [data],
         isLoading: false,
-    });
+        error: false,
+      }));
+    } catch (err) {
+      setState((state) => ({
+        ...state,
+        isLoading: false,
+        error: true,
+      }));
+      throw err;
+    }
+  }, [name]);
 
-    const urlParams = useParams<UrlParamsType>();
+  useEffect(() => {
+    fetchCountry();
+  }, [fetchCountry]);
 
-    const fetchCountry = useCallback(async () => {
-
-        setState((state) => ({ ...state, isLoading: true }));
-        try {
-            const { data } = await Axios.get(baseURL + 'alpha/' + `${urlParams.name}`);
-            setState((state) => ({
-                ...state,
-                data: [data],
-                isLoading: false,
-                error: false,
-            }));
-        } catch (err) {
-            setState((state) => ({
-                ...state,
-                isLoading: false,
-                error: true,
-            }));
-            throw err;
-        }
-    }, [name]);
-
-    useEffect(() => {
-        fetchCountry();
-    }, [fetchCountry]);
-
-    return (
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
         <>
-            {isLoading ? <Loader /> :
-                <>
-                    {data &&
-                        data.map((country, index) => (
-                            <>
-                                <Nav name={country.name} key={index+'nav'} />
-                                <Container className="d-flex mb-5" key={index}>
-                                    <Col lg={6}>
-                                        <img className="country-flag" src={country.flag} />
-                                        <iframe className="country-map" src={`https://maps.google.com/maps?q=${country.name}&z=5&t=m&output=embed`} />
-                                    </Col>
-                                    <Col lg={6}>
-                                        <Row className="w-100 d-flex">
-                                            <Col lg={6} className="my-3">
-                                                <h1 className="text-center">{country.name}</h1>
-                                            </Col>
-                                            <Col lg={6} className="my-3">
-                                                <h1 className="text-center">{country.nativeName}</h1>
-                                            </Col>
-                                        </Row>
-                                        <Row>
-                                            <p>This country also called as {' '}
-                                                <span title='German'>{country.translations.de} in German, </span>
-                                                <span title='Spanish'>{country.translations.es} in Spanish, </span>
-                                                <span title='French'>{country.translations.fr} in French, </span>
-                                                <span title='Japanese'>{country.translations.ja} in Japanese, </span>
-                                                <span title='Italian'>{country.translations.it} in Italian, </span>
-                                                <span title='Persian'>{country.translations.fa} in Persian, </span>
-                                                <span title='Portuguese'>{country.translations.pt} in Portuguese, </span>
-                                                <span title='Dutch'>{country.translations.nl} in Dutch.</span>
-                                            </p>
-                                        </Row>
-                                        <hr />
-                                        <Row className="w-100">
-                                            <div className="card-columns">
-                                                <Card className="p-3 bg-dark text-white m-1 align-items-center justify-content-between">
-                                                        <h2 className="text-center">{country.capital}</h2>
-                                                        <h6 className="text-center">Capital</h6>
-                                                </Card>
-                                                <Card className="p-3 bg-dark text-white m-1 align-items-center justify-content-between">
-                                                    <h2 className="text-center">{country.subregion}</h2>
-                                                    <h6 className="text-center">Subregion</h6>
-                                                </Card>
-                                                <Card className="p-3 bg-dark text-white m-1 align-items-center justify-content-between">
-                                                        <h2 className="text-center">{country.region}</h2>
-                                                        <h6 className="text-center">Region</h6>
-                                                </Card>
-                                                <Card className="p-3 bg-dark text-white m-1 align-items-center justify-content-between">
-                                                    <h2 className="card-title text-center">{country.callingCodes}</h2>
-                                                    <h6 className="text-center">Calling Codes</h6>
-                                                </Card>
-                                                <Card className="p-3 bg-dark text-white m-1 align-items-center justify-content-between">
-                                                    <h2 className="card-title text-center">{country.alpha2Code}</h2>
-                                                    <h6 className="text-center">Alpha 2 Code</h6>
-                                                </Card>
-                                                <Card className="p-3 bg-dark text-white m-1 align-items-center justify-content-between">
-                                                    <h2 className="card-title text-center">{country.alpha3Code}</h2>
-                                                    <h6 className="text-center">Alpha 3 Code</h6>
-                                                </Card>
-                                                <Card className="p-3 bg-dark text-white m-1 align-items-center justify-content-between">
-                                                    <h2 className="card-title text-center">{country.topLevelDomain}</h2>
-                                                    <h6 className="text-center">Top Level Domain</h6>
-                                                </Card>
-                                                <Card className="p-3 bg-dark text-white m-1 align-items-center justify-content-between">
-                                                    <h2 className="card-title text-center">{country.currencies[0].name}</h2>
-                                                    <h6 className="text-center">Currency</h6>
-                                                </Card>
-                                            </div>
-                                        </Row>
-                                    </Col>
-                                </Container>
-                                <Footer />
-                            </>
-                        ))}
-                </>
-            }
+          {data &&
+            data.map((country, index) => (
+              <>
+                <Nav name={country.name} key={index + "nav"} />
+                <Container className="d-flex" key={index}>
+                  <Row className="my-5">
+                    <Col lg={6}>
+                      <img className="country-flag" src={country.flag} />
+                      <iframe
+                        className="country-map"
+                        src={`https://maps.google.com/maps?q=${country.name}&z=5&t=m&output=embed`}
+                      />
+                    </Col>
+                    <Col lg={6}>
+                      <Row className="w-100 d-flex">
+                        <Col lg={6} className="mb-3">
+                          <h1 className="text-center">{country.name}</h1>
+                        </Col>
+                        <Col lg={6} className="mb-3">
+                          <h1 className="text-center">{country.nativeName}</h1>
+                        </Col>
+                      </Row>
+                      <Row>
+                        <p>
+                          This country also called as{" "}
+                          <span title="German">
+                            {country.translations.de} in German,{" "}
+                          </span>
+                          <span title="Spanish">
+                            {country.translations.es} in Spanish,{" "}
+                          </span>
+                          <span title="French">
+                            {country.translations.fr} in French,{" "}
+                          </span>
+                          <span title="Japanese">
+                            {country.translations.ja} in Japanese,{" "}
+                          </span>
+                          <span title="Italian">
+                            {country.translations.it} in Italian,{" "}
+                          </span>
+                          <span title="Persian">
+                            {country.translations.fa} in Persian,{" "}
+                          </span>
+                          <span title="Portuguese">
+                            {country.translations.pt} in Portuguese,{" "}
+                          </span>
+                          <span title="Dutch">
+                            {country.translations.nl} in Dutch.
+                          </span>
+                        </p>
+                      </Row>
+                      <hr />
+                      <Row className="w-100">
+                        <div className="card-columns">
+                          <Card className="p-2 bg-dark text-white  align-items-center justify-content-between">
+                            <h1 className="card-title h4 text-center">
+                              {country.capital}
+                            </h1>
+                            <h6 className="text-center">Capital</h6>
+                          </Card>
+                          <Card className="p-2 bg-dark text-white  align-items-center justify-content-between">
+                            <h1 className="card-title h4 text-center">
+                              {country.subregion}
+                            </h1>
+                            <h6 className="text-center">Subregion</h6>
+                          </Card>
+                          <Card className="p-2 bg-dark text-white  align-items-center justify-content-between">
+                            <h1 className="card-title h4 text-center">
+                              {country.region}
+                            </h1>
+                            <h6 className="text-center">Region</h6>
+                          </Card>
+                          <Card className="p-2 bg-dark text-white  align-items-center justify-content-between">
+                            <h1 className="card-title h4 text-center">
+                              {country.callingCodes}
+                            </h1>
+                            <h6 className="text-center">Calling Codes</h6>
+                          </Card>
+                          <Card className="p-2 bg-dark text-white  align-items-center justify-content-between">
+                            <h1 className="card-title h4 text-center">
+                              {country.alpha2Code}
+                            </h1>
+                            <h6 className="text-center">Alpha 2 Code</h6>
+                          </Card>
+                          <Card className="p-2 bg-dark text-white  align-items-center justify-content-between">
+                            <h1 className="card-title h4 text-center">
+                              {country.alpha3Code}
+                            </h1>
+                            <h6 className="text-center">Alpha 3 Code</h6>
+                          </Card>
+                          <Card className="p-2 bg-dark text-white  align-items-center justify-content-between">
+                            <h1 className="card-title h4 text-center">
+                              {country.topLevelDomain}
+                            </h1>
+                            <h6 className="text-center">Top Level Domain</h6>
+                          </Card>
+                          <Card className="p-2 bg-dark text-white  align-items-center justify-content-between">
+                            <h1 className="card-title h4 text-center">
+                              {country.currencies[0].name}
+                            </h1>
+                            <h6 className="text-center">Currency</h6>
+                          </Card>
+                        </div>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Container>
+                <Footer />
+              </>
+            ))}
         </>
-    );
+      )}
+    </>
+  );
 };
--- src/pages/nav/index.tsx
+++ src/pages/nav/index.tsx
@@ -1,44 +1,111 @@
-import React, { FC } from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Lottie from 'react-lottie';
+import React, { FC } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Lottie from "react-lottie";
 import animationData from "../../assets/info-icon2.json";
-import './style.scss';
+import "./style.scss";
 import { Helmet } from "react-helmet";
-import ReactGA from 'react-ga';
+import ReactGA from "react-ga";
 
-ReactGA.initialize('UA-173040299-1');
+ReactGA.initialize("UA-173040299-1");
+const location = window.location.href;
 
 type Props = {
-    name?: string
-}
+  name?: string;
+};
 
 export const Nav: FC<Props> = ({ name }) => {
-    ReactGA.pageview(window.location.pathname);
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        rendererSettings: {
-            preserveAspectRatio: 'xMidYMid slice'
-        }
-    }
-    return (
-        <>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>{name ? `Everything About ${name} | everythingabout.xyz` : "everythingabout.xyz | basic info about all countries"}</title>
-                <link rel="canonical" href="http://everythingabout.xyz/" />
-            </Helmet>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top" className="d-flex justify-content-center text-center">
-                <Navbar.Brand href="/">
-                    <div className="d-flex">
-                        <Lottie options={defaultOptions}
-                            height={30}
-                            width={30} />
-                        <span className="px-1 brand-text">{name ? `Everything About ${name}` : "everythingabout.xyz"}</span>
-                    </div>
-                </Navbar.Brand>
-            </Navbar>
-        </>
-    )
-}
+  ReactGA.pageview(window.location.pathname);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+
+        <title>
+          {name
+            ? `Everything About ${name} | everythingabout.xyz`
+            : "everythingabout.xyz | basic info about all countries"}
+        </title>
+        <meta
+          name="title"
+          content={
+            name
+              ? `Everything About ${name} | everythingabout.xyz`
+              : "everythingabout.xyz | basic info about all countries"
+          }
+        />
+        <meta
+          name="description"
+          content={
+            name
+              ? `Everything About ${name} | everythingabout.xyz`
+              : "everythingabout.xyz | basic info about all countries"
+          }
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={location} />
+        <meta
+          property="og:title"
+          content={
+            name
+              ? `Everything About ${name} | everythingabout.xyz`
+              : "everythingabout.xyz | basic info about all countries"
+          }
+        />
+        <meta
+          property="og:description"
+          content={
+            name
+              ? `Everything About ${name} | everythingabout.xyz`
+              : "everythingabout.xyz | basic info about all countries"
+          }
+        />
+
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:url" content={location} />
+        <meta
+          property="twitter:title"
+          content={
+            name
+              ? `Everything About ${name} | everythingabout.xyz`
+              : "everythingabout.xyz | basic info about all countries"
+          }
+        />
+        <meta
+          property="twitter:description"
+          content={
+            name
+              ? `Everything About ${name} | everythingabout.xyz`
+              : "everythingabout.xyz | basic info about all countries"
+          }
+        />
+        <link rel="canonical" href={location} />
+      </Helmet>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        sticky="top"
+        className="d-flex justify-content-center text-center"
+      >
+        <Navbar.Brand href="/">
+          <div className="d-flex">
+            <Lottie options={defaultOptions} height={30} width={30} />
+            <span className="px-1 brand-text">
+              {name ? `Everything About ${name}` : "everythingabout.xyz"}
+            </span>
+          </div>
+        </Navbar.Brand>
+      </Navbar>
+    </>
+  );
+};
